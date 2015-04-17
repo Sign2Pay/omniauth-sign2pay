dea24a9328339dcbd3fe52ae7addc1886e2ca8db
Stephanie Nemeth <traumverloren@gmail.com>
2015-04-17 14:48:12 +0200

    working on strategy

--- lib/omniauth/strategies/sign2pay.rb
+++ lib/omniauth/strategies/sign2pay.rb
@@ -1,7 +1,32 @@
-require "omniauth-sign2pay"
+require "omniauth"
 
 module OmniAuth
-  module Sign2pay
-    # Your code goes here...
+  module Strategies
+    class Sign2pay
+      include OmniAuth::Strategy
+
+      # args receives parameters from the strategy declaration in client's rack middleware
+      args [:client_id, :client_secret, :redirect_uri]
+
+      option :name, "sign2pay"
+
+      # redirect to sign2pay server site for authentication of client
+      def request_phase
+        redirect :redirect_uri
+      end
+
+      # after authentication with sign2pay, redirect to client and send along info about client
+      def callback_phase
+      end
+
+      uid { request.params['user_id'] }
+
+      info do
+        {
+          :email => request.params['email']
+        }
+      end
+
+    end
   end
 end
